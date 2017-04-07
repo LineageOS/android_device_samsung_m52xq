a0a3a7b5085e5578eacb2e7b2740e61d21410169
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    m52xq: overlay: Set default hotspot SSID prefix
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ $(call inherit-product, vendor/samsung/m52xq/m52xq-vendor.mk)
 PRODUCT_PACKAGES += \
     FrameworksResDevice \
     SettingsResDevice \
-    SystemUIResDevice
+    SystemUIResDevice \
+    WifiResDevice
 
 # Init files
 PRODUCT_PACKAGES += \
