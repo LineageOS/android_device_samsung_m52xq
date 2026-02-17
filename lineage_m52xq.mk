b724b59241ac5226928d9392943bcd2984d20ac5
Bush-cat <bushccat@gmail.com>
2026-02-17 12:38:42 +0000

    m52xq: update blobs from M526BRXXS7CYE1
    Change-Id: Id9b5664b5d3326fcfb14bfa4a833330e325a9b08

--- lineage_m52xq.mk
+++ lineage_m52xq.mk
@@ -49,5 +49,5 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Vendor fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint="samsung/m52xqins/m52xq:11/RP1A.200720.012/M526BXXS6CXJ2:user/release-keys" \
-    BuildDesc="m52xqins-user 11 RP1A.200720.012 M526BXXS6CXJ2 release-keys"
+    BuildFingerprint="samsung/m52xqins/m52xq:11/RP1A.200720.012/M526BRXXS7CYE1:user/release-keys" \
+    BuildDesc="m52xqins-user 11 RP1A.200720.012 M526BRXXS7CYE1 release-keys"
